c5304a1df2323e6706dff08e1165798828465c73
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-09-13 12:09:53 -0700

    Create Main_Driver.cpp

---
+++ Main_Driver.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math> //????
+#include "ReadyQueue.h"
+using namespace std; 
+
+int main{
+
+ReadyQueue q1, q2;
+
+q1.size(50);
+q2.size(100);
+
+q1.add(15);
+q1.add(6);
+//.
+//.
+//.
+
+
+q1.display();
+q1.remove();
+//......
+
+for(int i=0; i<100; i++){
+  int num = rand(23) % 50;
+  q2.add(num);
+  }
